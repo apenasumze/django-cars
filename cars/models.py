cee8232fa824dc9f4cc74b6ac74679c334428e55
José Vitor Coelho <jvitorac123@hotmail.com>
2026-04-21 22:49:46 -0300

    Avanço em CBV, signals e alteração do database para Postgres

--- cars/models.py
+++ cars/models.py
@@ -18,6 +18,7 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True) # Placa do veiculo
     value = models.FloatField(blank=True, null=True) # Valor do veiculo
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True) # Descricao do veiculo
     
     def __str__(self):
         return self.model
--- cars/signals.py
+++ cars/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
@@ -7,6 +7,11 @@ def car_inventory_update():
     cars_count = Car.objects.all().count()
     cars_value = Car.objects.aggregate(total_value=Sum('value'))['total_value'] or 0
     CarInventory.objects.create(cars_count=cars_count, cars_value=cars_value)
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = f"{instance.model} - {instance.brand.name} - {instance.factory_year}/{instance.model_year}" 
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
